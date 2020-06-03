c3bffde6073f7ad0739887c3175230e61a7d7833
evgenii-code <osipenkov.evgenii@gmail.com>
2020-06-03 22:16:33 +0300

    Charts init

--- pages/charts.vue
+++ pages/charts.vue
@@ -3,6 +3,8 @@
     <container>
       <section class="charts">
         <h1 class="charts__title">Crypto charts</h1>
+
+        <line-chart :data="chartData" :options="chartOptions" :height="200" />
       </section>
     </container>
   </main>
@@ -10,6 +12,7 @@
 
 <script>
 import Container from '@/components/Container';
+import LineChart from '@/components/LineChart';
 import mixinComponents from '@/mixins/mixinComponents';
 
 export default {
@@ -17,6 +20,82 @@ export default {
 
   components: {
     container: Container,
+    LineChart,
+  },
+
+  data() {
+    return {
+      chartData: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        datasets: [
+          {
+            backgroundColor: '#f2a365',
+            data: [40, 15, 20, 25, 50, 100],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+
+        legend: {
+          display: false,
+        },
+
+        title: {
+          display: true,
+          text: 'BTC rates',
+          fontColor: '#ececec',
+        },
+
+        hover: {
+          intersect: false,
+        },
+
+        tooltips: {
+          intersect: false,
+          mode: 'x-axis',
+          displayColors: false,
+          titleAlign: 'center',
+          caretSize: 5,
+          caretPadding: 5,
+        },
+
+        elements: {
+          point: {
+            radius: 0,
+            hitRadius: 6,
+            borderWidth: 0,
+            borderColor: 'rgba(0, 0, 0, 0)',
+          },
+        },
+
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                fontColor: '#ececec',
+              },
+
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              ticks: {
+                fontColor: '#ececec',
+              },
+
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+      },
+    };
   },
 };
 </script>
